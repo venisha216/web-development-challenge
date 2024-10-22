d98a1926071e0fe60fb51598ebc3701fa67f9f49
venisha216 <venisha2185@gmail.com>
2024-10-22 06:46:51 +0530

    challenge 2 completed

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,45 @@
-const express = require("express")
-const app = express()
-app.use(express.json())
+//step 2: connecting to express
+const express = require("express");
+const app = express();
+const cors = require('cors');
+const port = process.env.port || 5000;
+app.use(express.json());
+app.use(cors());
+app.listen(port,()=>{
+    console.log(`Server is running at port http://localhost:${port}`);
+});
+const mongoose = require ('mongoose');
 
-app.listen(5000,()=>{
-    console.log("Server is listening at port 5000");
-})
+//step 1: connect to Database
+mongoose.connect('mongodb://localhost/productsDB').then(()=> console.log("MongoDB connected")).catch(err=>console.log("MongoDB connection error:",err));
+
+//step 3: defining product schema and model
+
+const productSchema = new mongoose.Schema({
+    product_id: String,
+    name: String,
+    price: Number,
+    description: String,
+    stock: Number,
+    category: String
+});
+const Product = mongoose.model('Product', productSchema);
+
+//step 4: setting root url
+app.get('/',(req,res)=>res.send("Welcome to the Products Page!"));
+
+//step 5: create route to fetch product data from DB
+// Route to get all products
+app.get('/products', async (req, res) => {
+    try {
+        const products = await Product.find(); 
+        res.json(products);
+    } catch (error) {
+        res.status(500).json({ message: 'Error while fetching products' });
+    }
+});
+
+
+
+
+
